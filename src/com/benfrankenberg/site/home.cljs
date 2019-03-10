528e8976f962495b3e42f54bc712e6761b555e30
Jay Zawrotny <jayzawrotny@gmail.com>
2019-03-10 15:51:48 -0400

    Fixed site title to match headline

--- src/com/benfrankenberg/site/home.cljs
+++ src/com/benfrankenberg/site/home.cljs
@@ -5,10 +5,10 @@
   '(["!DOCTYPE", "html"]
     [:html
       [:head
-        [:title "Benjamin Frankenberg: Stuntman, Swordsman, Actor"]
+        [:title "Benjamin Frankenberg: Actor, Artist, Swordsman"]
         [:meta {:charset "utf-8"}]
-        [:meta {:http-equiv "X-UA-Compatible"}]
-                :content "IE=edge,chrome=1"
+        [:meta {:http-equiv "X-UA-Compatible"
+                :content "IE=edge,chrome=1"}]
         [:meta {:name "viewport"
                 :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
         [:link {:rel "stylesheet"
