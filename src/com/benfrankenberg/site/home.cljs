10afc2acd9461009de94239e94d1eda6ab78bc81
Jay <jayzawrotny@gmail.com>
2019-03-11 15:44:21 -0400

    Setup Ben's preferred font and replaced hero image

--- src/com/benfrankenberg/site/home.cljs
+++ src/com/benfrankenberg/site/home.cljs
@@ -12,6 +12,8 @@
         [:meta {:name "viewport"
                 :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
         [:link {:rel "stylesheet"
+                :href "https://use.typekit.net/goa5jvn.css"}]
+        [:link {:rel "stylesheet"
                 :href "/css/style.css"}]
         [:link {:rel "stylesheet"
                 :href "https://fonts.googleapis.com/css?family=IM+Fell+English|Slabo+27px"}]
