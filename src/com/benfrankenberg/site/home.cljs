c8b2899afd48a8e3148f23183ddfad92bb606899
Jay Zawrotny <jayzawrotny@gmail.com>
2019-03-16 17:46:37 -0400

    Added Resume link

--- src/com/benfrankenberg/site/home.cljs
+++ src/com/benfrankenberg/site/home.cljs
@@ -1,8 +1,47 @@
 (ns src.com.benfrankenberg.site.home)
 
+(defn background
+  []
+  [:div.background nil])
+
+(defn hero
+  []
+  [:header.hero
+    [:div.hero__inner
+      [:h1.hero__title.display "Benjamin Frankenberg"]
+      [:h2.hero__subtitle.body "Actor &bull; Artist &bull; Swordsman"]]])
+
+(defn bio
+  []
+  [:div.group.bio
+    [:h3.display "About Ben"]
+    [:p.text.mt "Ben is a professional actor with experience in a wide selection of mediums such as theater, film, TV, and movies. He breathes life into characters by incorporating both technical acting techniques and his personal artistic vision to delight audiences on both the macro and micro level to reach deep within their mind and souls. Ben currently holds a performance art degree from Savannah College School of Art and Design."]])
+
+(defn links
+  []
+  [:div.group.links
+    [:a.btn.links__link
+      {:href "/downloads/benjamin-frankenberg-resume.pdf"}
+      [:i.inline.fa.fa-file-pdf]
+      "Download Resume"]
+    [:a.btn.links__link
+      {:href "https://www.imdb.com/name/nm5528224/"}
+      [:i.inline.fab.fa-imdb]
+      "View Credits"]])
+
+(defn about
+  []
+  [:section.section.about
+    [:div.column.about__content
+      (bio)
+      (links)]
+    [:div.column.about__headshot.desktop
+      [:div.profile-photo]]])
+
 (defn render
   []
-  '(["!DOCTYPE", "html"]
+  (list
+    ["!DOCTYPE", "html"]
     [:html
       [:head
         [:title "Benjamin Frankenberg: Actor, Artist, Swordsman"]
@@ -12,9 +51,12 @@
         [:meta {:name "viewport"
                 :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
         [:link {:rel "stylesheet"
+                :href "https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+                :crossorigin "anonymous"}]
+        [:link {:rel "stylesheet"
                 :href "/css/style.css"}]
         [:link {:rel "stylesheet"
-                :href "https://fonts.googleapis.com/css?family=Alegreya|Slabo+27px"}]
+                :href "https://fonts.googleapis.com/css?family=Alegreya|Raleway"}]
         [:link {:rel "stylesheet"
                  :media "screen and (max-width: 929px)"
                  :href "/css/mobile.css"}]
@@ -25,8 +67,8 @@
                 :media "screen and (min-width: 930px)"
                 :href "/css/desktop.css"}]]
       [:body
-        [:header.hero
-          [:div.hero__inner
-            [:h1.hero__title.display "Benjamin Frankenberg"]
-            [:h2.body "Actor &bull; Artist &bull; Swordsman"]]]]]))
+        [:div.page
+          (list (background)
+                (hero)
+                (about))]]]))
 
