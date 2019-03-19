715a42ff4aa882b31c403d6451894f0e98e64571
Jay Zawrotny <jayzawrotny@gmail.com>
2019-03-18 20:33:06 -0400

    Refactored scripts into cmd lib

---
+++ src/com/benfrankenberg/tasks/lib/cmd.cljs
@@ -0,0 +1,71 @@
+(ns src.com.benfrankenberg.tasks.lib.cmd
+  (:require
+    [clojure.string :as s]
+    [src.com.benfrankenberg.tasks.lib.color :as c]
+    [src.com.benfrankenberg.tasks.lib.stream :as stream]
+    [src.com.benfrankenberg.tasks.lib.util :refer [obj->clj]]))
+
+(def cp (js/require "child_process"))
+(def log (js/require "fancy-log"))
+(def stream (js/require "@eccentric-j/highland"))
+
+(defn tag-stream
+  [type source]
+  (-> source
+      (.pipe (stream))
+      (.map #(.toString % "utf-8"))
+      (.split)
+      (.compact)
+      (.map #(hash-map :type type :data %))))
+
+(defn stream-exit
+  [type event-name source]
+  (-> (stream event-name source #js ["code" "signal"])
+      (.take 1)
+      (.map obj->clj)
+      (.map #(hash-map :type type :data %))))
+
+(defn streamify
+  [{:keys [child stdout stdin stderr]}]
+  (-> (stream #js [(tag-stream :stdout stdout)
+                   (tag-stream :stderr stderr)
+                   (stream-exit :exit "exit" child)])
+      (.merge)))
+
+(defn exec
+  [cmd-str]
+  (log (c/line (c/plugin "scripts")
+               "Executing"
+               (c/file cmd-str)))
+  (let [[cmd & args] (s/split cmd-str " ")
+        child (.spawn cp cmd (clj->js args) #js {:stdio "pipe"})]
+    (streamify {:child child
+                :stdout (.-stdout child)
+                :stdin (.-stdin child)
+                :stderr (.-stderr child)})))
+
+(defn on-stdout
+  [f]
+  (fn [update]
+    (when (= (:type update) :stdout)
+      (f (:data update))
+      update)))
+
+(defn on-stderr
+  [f]
+  (fn [update]
+    (when (= (:type update) :stderr)
+      (f (:data update))
+      update)))
+
+(defn on-exit
+  [f]
+  (fn [update]
+    (when (= (:type update) :exit)
+      (f (:data update))
+      update)))
+
+(defn until-exit
+  [source]
+  (-> source
+      (.consume (stream/take-while #(not= (:type %) :exit)))))
--- src/com/benfrankenberg/tasks/scripts.cljs
+++ src/com/benfrankenberg/tasks/scripts.cljs
@@ -1,77 +1,18 @@
 (ns src.com.benfrankenberg.tasks.scripts
   (:require
     [clojure.string :as s]
+    [src.com.benfrankenberg.tasks.lib.cmd :as cmd]
     [src.com.benfrankenberg.tasks.lib.color :as c]
     [src.com.benfrankenberg.tasks.lib.stream :as stream]
-    [src.com.benfrankenberg.tasks.lib.util :refer [base glob? obj->clj rename]]))
+    [src.com.benfrankenberg.tasks.lib.util :refer [obj->clj]]))
 
-(def Buffer (.-Buffer (js/require "buffer")))
-(def cp (js/require "child_process"))
 (def gulp (js/require "gulp"))
 (def log (js/require "fancy-log"))
-(def stream (js/require "@eccentric-j/highland"))
-(def Vinyl (js/require "vinyl"))
 
 (defn log-file
   [file]
   (println {:path (.-path file)
             :relative (.-relative file)}))
-
-(defn tag-stream
-  [type source]
-  (-> source
-      (.pipe (stream))
-      (.map #(.toString % "utf-8"))
-      (.split)
-      (.compact)
-      (.map #(hash-map :type type :data %))))
-
-(defn stream-exit
-  [type event-name source]
-  (-> (stream event-name source #js ["code" "signal"])
-      (.take 1)
-      (.map obj->clj)
-      (.map #(hash-map :type type :data %))))
-
-(defn streamify
-  [{:keys [child stdout stdin stderr]}]
-  (-> (stream #js [(tag-stream :stdout stdout)
-                   (tag-stream :stderr stderr)
-                   (stream-exit :exit "exit" child)])
-      (.merge)))
-
-(defn exec
-  [cmd-str]
-  (log (c/line (c/plugin "scripts")
-               "Executing"
-               (c/file cmd-str)))
-  (let [[cmd & args] (s/split cmd-str " ")
-        child (.spawn cp cmd (clj->js args) #js {:stdio "pipe"})]
-    (streamify {:child child
-                :stdout (.-stdout child)
-                :stdin (.-stdin child)
-                :stderr (.-stderr child)})))
-
-(defn on-stdout
-  [f]
-  (fn [update]
-    (when (= (:type update) :stdout)
-      (f (:data update))
-      update)))
-
-(defn on-stderr
-  [f]
-  (fn [update]
-    (when (= (:type update) :stderr)
-      (f (:data update))
-      update)))
-
-(defn on-exit
-  [f]
-  (fn [update]
-    (when (= (:type update) :exit)
-      (f (:data update))
-      update)))
 
 (defn log-stdout
   [output]
@@ -94,26 +35,18 @@
 
 (defn cljs->js
   [_]
-  (-> (exec "npx shadow-cljs watch :app")
-      (.tap (on-stdout log-stdout))
-      (.tap (on-stderr log-stderr))
-      (.tap (on-exit log-exit))
-      (.consume (stream/take-while #(not= (:type %) :exit)))
-      (.reject (constantly true))
-      (.errors
-        (fn [err push]
-          (.error js/console err)
-          (push err)))))
+  (-> (cmd/exec "npx shadow-cljs watch :app")
+      (.tap (cmd/on-stdout log-stdout))
+      (.tap (cmd/on-stderr log-stderr))
+      (.tap (cmd/on-exit log-exit))
+      (cmd/until-exit)
+      (.reject (constantly true))))
 
 (.task gulp "scripts"
   (fn []
-    (-> (exec "npx shadow-cljs release :app")
-        (.tap (on-stdout log-stdout))
-        (.tap (on-stderr log-stderr))
-        (.tap (on-exit log-exit))
-        (.consume (stream/take-while #(not= (:type %) :exit)))
-        (.errors
-          (fn [err push]
-            (.error js/console err)
-            (push err))))))
+    (-> (cmd/exec "npx shadow-cljs release :app")
+        (.tap (cmd/on-stdout log-stdout))
+        (.tap (cmd/on-stderr log-stderr))
+        (.tap (cmd/on-exit log-exit))
+        (cmd/until-exit))))
 
