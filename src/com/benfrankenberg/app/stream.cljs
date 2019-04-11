0e87d608beb94303168dc400db2d8f1f8443b1f8
Jay <jayzawrotny@gmail.com>
2019-04-11 14:11:35 -0400

    Documented stream and swipe app modules

--- src/com/benfrankenberg/app/stream.cljs
+++ src/com/benfrankenberg/app/stream.cljs
@@ -2,12 +2,79 @@
   (:require
    [bacon :as bacon :refer [End]]))
 
+(defn bind-event
+  "
+  Create an event stream that can control the bubbling of events.
+  For instance listening to touchmove events on the window and canceling them
+  if we detect a horizontal swipe.
+  Takes an event-name stream.
+  Returns a function to bind an active event listener.
+
+  Example:
+  (-> (stream/on js/window (bind-event \"touchmove\"))
+      (.doAction #(.preventDefault %))
+      (.log))
+  "
+  [event-name]
+  (fn [binder listener]
+    (binder event-name listener #js {:passive false})))
+
+(defn do-when
+  "
+  Perform a side-effect only if a side-effect function is callable.
+  Takes a source bacon stream and a function or falsey value.
+  Returns a bacon stream.
+
+  Example:
+  (-> (from [1 2 3])
+      (do-when (.-log js/console)) ;; logs each value
+      (.subscribe))
+  (-> (from [1 2 3])
+      (do-when (.-logger js/window)) ;; returns a valid stream but doesn't log
+      (.subscribe))
+  "
+  [source f]
+  (if (fn? f)
+    (.doAction source f)
+    source))
+
 (defn from
-  [source]
-  (cond (sequential? source)       (from (clj->js source))
-        (.isArray js/Array source) (.fromArray bacon source)
-        :else                      (from [source])))
+  "
+  Create a bacon stream from an iterable clojure object or js array.
+  Takes a array, list, vector, or hash-map.
+  Returns a Bacon stream.
+
+  Example:
+  (-> (stream/from [1 2 3])
+      (.log))
+  "
+  [values]
+  (cond (sequential? values)       (from (clj->js values))
+        (.isArray js/Array values) (.fromArray bacon values)
+        :else                      (from [values])))
 
 (defn of
-  [source]
-  (.once bacon source))
+  "
+  Creates a bacon stream that emits a single value.
+  Takes a value to push to a stream.
+  Returns a Bacon stream.
+
+  Example:
+  (-> (stream/of (.now js/Date))
+      (.log))
+  "
+  [value]
+  (.once bacon value))
+
+(defn on
+  "
+  Stream a DOM event listener.
+  Takes a subject and an event name string.
+  Returns a bacon stream.
+
+  Example:
+  (-> (stream/on js/window \"scroll\")
+      (.log))
+  "
+  [subject event-name]
+  (.fromEvent bacon subject event-name))
--- src/com/benfrankenberg/app/swipe.cljs
+++ src/com/benfrankenberg/app/swipe.cljs
@@ -1,16 +1,17 @@
 (ns com.benfrankenberg.app.swipe
-  (:require [bacon :as bacon]))
-
-(defn do-when
-  [source f]
-  (if f (.doAction source f) source))
-
-(defn bind-event
-  [event-name]
-  (fn [binder listener]
-    (binder event-name listener #js {:passive false})))
+  (:require
+   [bacon :as bacon]
+   [com.benfrankenberg.app.stream :refer [bind-event do-when]]))
 
 (defn normalize-touch
+  "
+  Takes a js touch event
+  Returns a hash map with :x and :y coords relative to the page
+
+  Example:
+  (normalize-touch a-touch-move-event)
+  ;; => {:x 300 :y 500}
+  "
   [event]
   (as-> event $
         (.-changedTouches $)
@@ -20,6 +21,20 @@
                   :y (.-pageY $))))
 
 (defn events->gesture
+  "
+  Takes an element and a vector of a touchstart and a touchmove or touchend
+  event like [touchstart touchmove] or [touchstart touchend].
+
+  Returns a hash-map to describe the gesture:
+  :events vector     - Vector of touchstart and touch move\\end events.
+  :el        Element - Element being swiped on
+  :h         Number  - Amount of horizontal distance between touch events.
+  :v         Number  - Amount of vertical distance between touch events.
+  :ratio     Number  - Ratio of v / h
+  :scale     Number  - h / 100 to return a 0–1 percent of full 100px swipe
+  :direction keyword - :prev or :next depending on horizontal change
+  :selector  str     - \".prev\" or \".next\"
+  "
   [el [start end]]
   (let [width (.-clientWidth el)
         {start-y :y start-x :x} (normalize-touch start)
@@ -37,16 +52,38 @@
      :selector (str "." (name direction))}))
 
 (defn swiping?
+  "
+  Predicate to determine if gesture is swiping.
+  Takes a gesture hash-map.
+  Returns true if horizontal distance is greater than 10px.
+  "
   [{:keys [v h]}]
   (> h 10))
 
 (defn swipe?
+  "
+  Predicate to determine if gesture was a full swipe
+  Takes a gesture hash-map.
+  Returns true if horizontal distance is greater than vertical. If the ratio
+  between horz and vert is greater than 1 and the horizontal distance is more
+  than 100px.
+  "
   ([{:keys [v h ratio]}]
    (and (> h v)
         (< ratio 1)
         (>= h 100))))
 
 (defn touch-end
+  "
+  Takes a hash-map of opts:
+  :block-scroll? atom     - Holds a boolean value used to prevent user from
+                            scrolling while swiping.
+  :el            Element  - DOMElement to listen for touchend events
+  :on-end        function - A side-effect function for responding to touchend
+                            events from the element.
+
+  Returns a Bacon stream of opts for one touch-end.
+  "
   [{:keys [block-scroll? el on-end] :as opts}]
   (-> (.fromEvent bacon el (bind-event "touchend"))
       (.take 1)
@@ -55,6 +92,16 @@
       (do-when on-end)))
 
 (defn touch-move
+  "
+  Takes a hash-map of opts:
+  :block-scroll? atom     - Holds a boolean value used to prevent user from
+                            scrolling while swiping.
+  :el            Element  - DOMElement to listen for touchmove events
+  :on-move       function - A side-effect function for responding to touchmove
+                            events from the element.
+
+  Returns a Bacon stream of touchmove events.
+  "
   [{:keys [block-scroll? el on-move]} start]
   (-> (.fromEvent bacon el (bind-event "touchmove"))
       (.map #(events->gesture el [start %]))
@@ -63,11 +110,29 @@
       (do-when on-move)))
 
 (defn touch-start
+  "
+  Takes a hash-map of opts:
+  :el       Element  - DOMElement to listen for touchstart events
+  :on-start function - Side-effect function for responding to touchstart
+                       events from the element.
+
+  Returns a Bacon stream of touchstart events.
+  "
   [{:keys [el on-start]}]
   (-> (.fromEvent bacon el (bind-event "touchstart"))
       (do-when on-start)))
 
 (defn global-touch-handler
+  "
+  Takes a hash-map of opts:
+  :block-scroll? atom     - Holds a boolean value used to prevent user from
+                            scrolling while swiping.
+
+  If the boolean held in block-scroll? is truthy we prevent the user from
+  scrolling the page while they are swiping.
+
+  Returns an empty bacon stream. Side-effects only.
+  "
   [{:keys [block-scroll?]}]
   (-> (.fromEvent bacon js/window (bind-event "touchmove"))
       (.filter #(= @block-scroll? true))
@@ -75,6 +140,21 @@
       (.filter false)))
 
 (defn swipe
+  "
+  General swipe detection handler.
+
+  Takes a map of opts:
+  Takes a hash-map of opts:
+  :el       Element  - DOMElement to listen for touchmove events
+  :on-start function - Side-effect function for responding to touchstart
+                       events from the element.
+  :on-move  function - Side-effect function for responding to touchmove
+                       events from the element.
+  :on-end   function - Side-effect function for responding to touchend
+                       events from the element.
+
+  Returns a Bacon stream of swipe gesture hash-maps.
+  "
   [{:keys [el on-start on-move on-end] :as opts}]
   (let [block-scroll? (atom false)
         opts (assoc opts :block-scroll? block-scroll?)]
