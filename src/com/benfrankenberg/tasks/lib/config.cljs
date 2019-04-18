6c9d36bd22c875daa12b75aee1eabe2aeef44f94
Jay <jayzawrotny@gmail.com>
2019-04-18 15:25:30 -0400

    Documented color, config, and stream.cljs tasks.lib.* files

--- src/com/benfrankenberg/tasks/lib/config.cljs
+++ src/com/benfrankenberg/tasks/lib/config.cljs
@@ -1,4 +1,8 @@
-(ns src.com.benfrankenberg.tasks.lib.config)
+(ns src.com.benfrankenberg.tasks.lib.config
+  "
+  Provides an env variable to refer to the build environment.
+  Values will either be :development or the keyword value of NODE_ENV.
+  ")
 
 (def env (-> js/process
              (.-env)
